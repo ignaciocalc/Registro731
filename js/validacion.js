2f3d20b086d76a3792b259b6f3a50d59de2a9250
lucianorobinson <lucianorobinson11@gmail.com>
2025-08-13 14:56:57 -0300

    Checkbox verificacion

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,10 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+ document.getElementById('regBtn').addEventListener('click', function() {
+    const checkbox = document.getElementById('terminos').value.trim();
+    if (!checkbox.checked) {
+      showAlertError();
+    }
+ });
