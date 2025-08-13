4c6acd53794592e57ef929c4a54116fc1f25aed7
alexsanchez <alexsanchez72006@gmail.com>
2025-08-13 14:30:46 -0300

    Contraseña 6 letras

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+document.getElementById("regBtn").addEventListener("click", function(e) {
+    e.preventDefault(); 
+
+    let password1 = document.getElementById("password1").value.trim();
+    let password2 = document.getElementById("password2").value.trim();
+
+    if (password1.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres.");
+        showAlertError();
+        return;
+    }
+
+
+    showAlertSuccess();
+});
